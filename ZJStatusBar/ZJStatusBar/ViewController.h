e03e57245158040b30312482804ede00c37d8a90
Zijia Zhai <zijiazhai@gmail.com>
2016-06-06 14:58:01 -0500

    First commit

---
+++ ZJStatusBar/ZJStatusBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZJStatusBar
+//
+//  Created by zijia on 6/6/.
+//  Copyright (c) 2016 zijia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZJStatusBar/ZJStatusBar/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ZJStatusBar
+//
+//  Created by zijia on 6/6/.
+//  Copyright (c) 2016 zijia. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
